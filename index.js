15d816f966f850f6dc1f7aef12d8f4d6082c6306
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-03 08:24:14 +0100

    Added a responder for serverside response

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 
 var http = require('http');
 var employeeService = require('./lib/emplyees');
+var responder = require('./lib/responseGenerator');
+var staticFile = responder.staticFile('/public');
 
-http.createServer(function (req, res){
+
+http.createServer(function (req, res) {
 
     // A parsed url to work with in case there are parameters
     var _url;
+
     // In case the client uses lower case for methods.
     req.method = req.method.toUpperCase();
-    console.log(req.method + '' +req.url);
-    res.end('Current time is ' + Date.now());
+    console.log(req.method + ' ' + req.url);
 
-    if(req.method != 'GET'){
-        res.writeHead(501, {'Content-type': 'plain-txt'});
-        return res.end(req.method + ' is not supported by this server');
+    if (req.method !== 'GET') {
+        res.writeHead(501, {
+            'Content-Type': 'text/plain'
+        });
+        return res.end(req.method + ' is not implemented by this ➥server.');
     }
-
-
-    if(_url = /^\/employees$/i.exec(req.url)){
-        employeeService.getEmployees(function(error, list){
-           if(error){
-               //send a 500 error
-           }
-           //send the list with a 200 status code
+    if (_url = /^\/employees$/i.exec(req.url)) {
+        employeeService.getEmployees(function (error, data) {
+            if (error) {
+                return responder.send500(error, res);
+            }
+            return responder.sendJson(data, res);
         });
-
-    }else if(_url = /^\/employees\/(\d+)$/i.exec(req.url)){
-        employeeService.getEmployee(_url[1], function(error, element){
-           if(error){
-               //send a 500 error
-           }
-           if(!data){
-               //send a 404 error
-           }
-           //send the element with a 200 status code
+    } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
+        employeeService.getEmployee(_url[1], function (error, data) {
+            if (error) {
+                return responder.send500(error, res);
+            }
+            if (!data) {
+                return responder.send404(res);
+            }
+            return responder.sendJson(data,res);
         });
-
-    }else {
-        //try to send a static file if exists
-        //otherwise, send a 404
-        }
-
-
-}).listen(3010);
+    }
+    else {
+        // try to send the static file
+        res.writeHead(200);
+        res.end('static file maybe');
+    }
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
